47ad098f0f4a3b03ec01fc96d1681ea602246e04
Harsh M <harshmaroo123@gmail.com>
2022-01-04 14:04:06 +0530

    added database testing methods in homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,34 +1,75 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/models/scorecard_model.dart';
+import 'package:flutter_application_1/services/database_service.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    Constants.user = FirebaseAuth.instance.currentUser as User;
-    Constants.userId = Constants.user!.uid;
-    return Container(
-      color: Colors.blue.shade100,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            Constants.userId,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
+    return SafeArea(
+      child: Material(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              Constants.userId,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                },
+                child: const Text("Sign out")),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text("Write habit"),
+              onPressed: () {
+                DbService.instance.setScorecard(
+                  Scorecard(
+                      habitName: "Ride",
+                      comment: "Joyriderrrrrrrrrrrrrrr",
+                      totalDays: 51,
+                      longestStreak: 345),
+                );
               },
-              child: const Text("Sign out"))
-        ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text("sync data"),
+              onPressed: () async {
+                await DbService.instance.syncScorecard();
+              },
+            ),
+            StreamBuilder(
+              stream: DbService.instance.getScorecards(),
+              builder: (context, snap) {
+                if (snap.hasData) {
+                  List<ListTile> listTiles = <ListTile>[];
+                  final cardsList = snap.data as List<Scorecard>;
+                  listTiles.addAll(
+                    cardsList.map(
+                      (sc) {
+                        return ListTile(
+                          leading: const Icon(Icons.crop_square_rounded),
+                          title: Text(sc.habitName),
+                          subtitle: Text(sc.comment),
+                        );
+                      },
+                    ),
+                  );
+                  return Expanded(
+                    child: ListView(
+                      children: listTiles,
+                    ),
+                  );
+                }
+                return const CircularProgressIndicator.adaptive();
+              },
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
 
+import '../constants.dart';
 import 'home_page.dart';
 
 class LandingPage extends StatelessWidget {
@@ -12,11 +13,13 @@ class LandingPage extends StatelessWidget {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
+        if (snapshot.connectionState == ConnectionState.active) {
           User? user = snapshot.data;
           if (user == null) {
             return const LoginPage();
           }
+          Constants.user = FirebaseAuth.instance.currentUser as User;
+          Constants.userId = Constants.user!.uid;
           return HomePage();
         } else {
           return const Scaffold(
